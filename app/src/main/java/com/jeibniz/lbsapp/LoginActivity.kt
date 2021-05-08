221cbed5eae68eb2e18456782fa72db1a5c0b7e6
bassam kaddoura <BsmKdd@outlook.com>
2021-05-08 15:52:25 +0200

    Two additional attacks

--- app/src/main/java/com/jeibniz/lbsapp/LoginActivity.kt
+++ app/src/main/java/com/jeibniz/lbsapp/LoginActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.room.Room
+import com.jeibniz.lbsapp.database.LbsRoomDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +15,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         findViewById<Button>(R.id.loginButton).setOnClickListener {
+            val dao = Room.databaseBuilder(
+                baseContext,
+                LbsRoomDatabase::class.java,
+                LbsRoomDatabase.DATABASE_NAME
+            ).build().getCreditCardDao()
+
+            GlobalScope.launch {
+                dao.deleteAll()
+            }
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
--- app/src/main/java/com/jeibniz/lbsapp/MainActivity.kt
+++ app/src/main/java/com/jeibniz/lbsapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.jeibniz.lbsapp
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -26,17 +28,35 @@ class MainActivity : AppCompatActivity() {
 
         loadDataFromDatabase()
 
+//         Secure screen solution start
+        window.setFlags(
+                        WindowManager.LayoutParams.FLAG_SECURE,
+                        WindowManager.LayoutParams.FLAG_SECURE);
+//         Secure screen solution end
+
+        // Saving the data
         findViewById<Button>(R.id.saveButton).setOnClickListener {
 
+            // Fetching the text
+            val cardNumber = findViewById<EditText>(R.id.creditCardNumber).text.toString()
+            val date = findViewById<EditText>(R.id.date).text.toString()
+            val cvv = Integer.parseInt(findViewById<EditText>(R.id.cvv).text.toString())
+
+            // To the shared preferences
+            val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+            with (sharedPref.edit()) {
+                putString(cardNumber, cardNumber)
+                putString(date, date)
+                putInt(cvv.toString(), cvv)
+                apply()
+            }
+
+            // To the database
             val dao = Room.databaseBuilder(
                 baseContext,
                 LbsRoomDatabase::class.java,
                 LbsRoomDatabase.DATABASE_NAME
             ).build().getCreditCardDao()
-
-            val cardNumber = findViewById<EditText>(R.id.creditCardNumber).text.toString()
-            val date = findViewById<EditText>(R.id.date).text.toString()
-            val cvv = Integer.parseInt(findViewById<EditText>(R.id.cvv).text.toString())
 
             val card = CreditCardEntity(1, cardNumber, date, cvv)
             GlobalScope.launch {
@@ -46,30 +66,32 @@ class MainActivity : AppCompatActivity() {
            Toast.makeText(this, "Data saved!", Toast.LENGTH_LONG).show()
         }
 
+        // Delete data from database on logout
         findViewById<Button>(R.id.logoutButton).setOnClickListener {
-            val dao = Room.databaseBuilder(
-                baseContext,
-                LbsRoomDatabase::class.java,
-                LbsRoomDatabase.DATABASE_NAME
-            ).build().getCreditCardDao()
-
-            val intent = Intent(this, LoginActivity::class.java)
-            GlobalScope.launch {
-                Log.d("TEST", "onCreate: Start")
-                delay(2000)
-                dao.deleteAll()
-
-                startActivity(intent)
-                finish()
-                Log.d("TEST", "onCreate: DONE")
-            }
+            deleteSensitiveData()
         }
 
+        // Simulating a crash/attack
         findViewById<TextView>(R.id.explainationText).setOnClickListener {
             throw RuntimeException("Intended crash")
         }
+
+        // Extreme solution, wiping data on crash
+
+//        Thread.setDefaultUncaughtExceptionHandler { _, _ ->
+//            val dao = Room.databaseBuilder(
+//                baseContext,
+//                LbsRoomDatabase::class.java,
+//                LbsRoomDatabase.DATABASE_NAME
+//            ).build().getCreditCardDao()
+//
+//            GlobalScope.launch {
+//                dao.deleteAll()
+//            }
+//        }
     }
 
+    // Auto filling data on login
     private fun loadDataFromDatabase() {
         val dao = Room.databaseBuilder(
             baseContext,
@@ -91,9 +113,28 @@ class MainActivity : AppCompatActivity() {
             }
 
             val numberOfCards= cardFlow.count { true }
-            Log.d("TEST", "loadDataFromDatabase: " + numberOfCards)
+            Log.d("TEST", "loadDataFromDatabase: $numberOfCards")
         }
+    }
 
 
+    // Function to delete the sensitive data from the database
+    private fun deleteSensitiveData(){
+        val dao = Room.databaseBuilder(
+            baseContext,
+            LbsRoomDatabase::class.java,
+            LbsRoomDatabase.DATABASE_NAME
+        ).build().getCreditCardDao()
+
+        val intent = Intent(this, LoginActivity::class.java)
+        GlobalScope.launch {
+            Log.d("TEST", "onCreate: Start")
+            delay(2000)
+            dao.deleteAll()
+
+            startActivity(intent)
+            finish()
+            Log.d("TEST", "onCreate: DONE")
+        }
     }
 }
